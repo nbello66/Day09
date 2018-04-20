b6f8a4b59adc9630446c13d76fde5ebc342300c9
nbello66 <bellonicholas66@gmail.com>
2018-04-20 09:09:13 +0200

    commit

---
+++ ex06/ft_destroy.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <ft_ultimator.h>
+
+void	ft_destroy(char ***factory)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (factory[i])
+	{
+		while (factory[i][j])
+		{
+			free(factory[i][j]);
+			j++;
+		}
+		free(factory[i]);
+		i++;
+	}
+	free(factory);
+}
